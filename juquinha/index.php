504e7cb75045fe533f0188f60f7b04c37453bb3e
JVPFV <99732249+JVPFV@users.noreply.github.com>
2022-05-09 15:54:24 -0300

    Update index.php

--- juquinha/index.php
+++ juquinha/index.php
@@ -3,7 +3,25 @@
     <div class="col-md-12"> 
         <div class="jumbotron"> 
 
+            
 <h2> Seja bem vindo ao meu novo tema <h2>  
+    <img src="assets/img/m1.png">
+    <h4>Primeira imagem aleatoria para o tema</h4> 
+    
+    <img src="assets/img/m2.png">
+    <h4>Segunda imagem aleatoria para o tema</h4> 
+    
+    <img src="assets/img/m3.png">
+    <h4>Terceira imagem aleatoria para o tema</h4> 
+    
+    <img src="assets/img/m4.png">
+    <h4>Quarta imagem aleatoria para o tema</h4> 
+    
+    <img src="assets/img/m5.png">
+    <h4>Quinta imagem aleatoria para o tema</h4> 
+    
+    <img src="assets/img/m6.png">
+    <h4>Sexta imagem aleatoria para o tema</h4> 
 
 </div>
 </div>
